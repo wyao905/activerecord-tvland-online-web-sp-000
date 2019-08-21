14595a22dc76c06b6a0588dc09a0e6d18c4c5be7
Warren Yao <warrenyca905@gmail.com>
2019-08-21 23:52:06 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,8 @@
 class CreateActors < ActiveRecord::Migrate[5.1]
   def change
     create_table :actors do |t|
-      t.string :
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
