c299a57b6d721b05921b9e1422276683c7e94e29
Warren Yao <warrenyca905@gmail.com>
2019-08-22 01:19:54 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
   
+  belongs_to :network
+  
+  def actors_list
+    list = []
+    self.actors.each{|actor| list << actor.first_name + " " + actor.last_name}
+    list
+  end
 end
